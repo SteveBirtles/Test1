d59d297bec33c6235f725927d6316c8111dd3539
Steve Birtles <sbirtles@farnborough.ac.uk>
2019-06-14 10:29:40 +0100

    Did stuff

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("This is my coursework, wooo!");
+        System.out.println("Welcome to my epic coursework project! Wooooo!");
 
     }
 
