14d5f0ad47da5ef3aa1ead2e87700c8728b4140a
Steve Birtles <sbirtles@farnborough.ac.uk>
2019-06-14 12:13:54 +0100

    I did some basic CRUD operations.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,12 @@
 public class Main {
 
+    /* This is my main method where the program execution begins! */
     public static void main(String[] args) {
 
         System.out.println("Welcome to my epic coursework project! Wooooo!");
 
     }
+
+
 
 }
